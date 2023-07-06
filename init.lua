79daaf37def35a1617207c096952a3d70d00f52f
Arijit Basu <sayanarijit@gmail.com>
2023-07-07 02:13:49 +0530

    Modernize the inactive pane

--- init.lua
+++ init.lua
@@ -3,99 +3,65 @@ local xplr = xplr
 ---@diagnostic enable
 
 local pane = {
-  LEFT = 1,
-  RIGHT = 2,
+  LEFT = "left",
+  RIGHT = "right",
 }
 
-local dual_pane = nil
+local state = {
+  active = nil,
+  inactive = nil,
+}
 
 local inactive_pane_layout = {
-  CustomContent = {
-    title = nil,
-    body = {
-      DynamicList = { render = "custom.dual_pane.render_inactive_pane" },
-    },
-  },
+  Dynamic = "custom.dual_pane.render_inactive_pane",
 }
 
-local function deepcopy(obj)
-  if type(obj) ~= "table" then
-    return obj
-  end
-  local res = {}
-  for k, v in pairs(obj) do
-    res[deepcopy(k)] = deepcopy(v)
-  end
-  return res
-end
-
-local function split_pane(
-  layout,
-  left_pane_width,
-  right_pane_width,
-  left_pane,
-  right_pane
-)
-  if layout == "Table" then
-    return {
-      Horizontal = {
-        config = {
-          constraints = {
-            left_pane_width,
-            right_pane_width,
-          },
-        },
-        splits = {
-          left_pane,
-          right_pane,
-        },
-      },
-    }
-  elseif layout.Horizontal or layout.Vertical then
-    local res = deepcopy(layout)
-    for _, v in pairs(res) do
-      for i, l in ipairs(v.splits) do
-        v.splits[i] = split_pane(
-          l,
+local function split_pane(left_pane_width, right_pane_width, left_pane, right_pane)
+  local layout = {
+    Horizontal = {
+      config = {
+        constraints = {
           left_pane_width,
           right_pane_width,
-          left_pane,
-          right_pane
-        )
-      end
-    end
-    return res
-  else
-    return layout
-  end
+        },
+      },
+      splits = {
+        left_pane,
+        right_pane,
+      },
+    },
+  }
+
+  return xplr.util.layout_replace(xplr.config.layouts.builtin.default, "Table", layout)
 end
 
-local function activate_pane(pane_, ctx)
-  local layout = "right_pane_active"
-  if pane_ == pane.LEFT then
-    layout = "left_pane_active"
+local function offset(listing, height, focus)
+  local h = height - 3
+  local start = (focus - (focus % h))
+  local result = {}
+  for i = start + 1, start + h, 1 do
+    table.insert(result, listing[i])
   end
+  return result
+end
 
-  if dual_pane == nil then
-    dual_pane = {
-      active = pane_,
-      inactive = {
-        directory_buffer = ctx.directory_buffer,
-        explorer_config = ctx.explorer_config,
-      },
+local function activate_pane(pane_, app)
+  local layout = pane_ .. "_pane_active"
+
+  if state.active == nil then
+    state.active = pane_
+    state.inactive = app
+    return {
+      { SwitchLayoutCustom = layout },
     }
-    return { { SwitchLayoutCustom = layout } }
-  end
+  elseif state.active ~= pane_ then
+    local pwd = state.inactive.pwd
+    local sorters = state.inactive.explorer_config.sorters
+    local filters = state.inactive.explorer_config.filters
+    local focus = (state.inactive.directory_buffer or {}).focus or 0
 
-  if dual_pane.active ~= pane_ then
-    local pwd = dual_pane.inactive.directory_buffer.parent
-    local focus = dual_pane.inactive.directory_buffer.focus
-    local sorters = dual_pane.inactive.explorer_config.sorters
-    local filters = dual_pane.inactive.explorer_config.filters
-
-    dual_pane.inactive.directory_buffer = deepcopy(ctx.directory_buffer)
-    dual_pane.inactive.explorer_config = deepcopy(ctx.explorer_config)
-    dual_pane.active = pane_
+    state.inactive = app
+    state.active = pane_
 
     local msgs = {
       "ClearNodeFilters",
@@ -113,9 +79,111 @@ local function activate_pane(pane_, ctx)
     table.insert(msgs, { ChangeDirectory = pwd })
     table.insert(msgs, { FocusByIndex = focus })
     table.insert(msgs, { SwitchLayoutCustom = layout })
-
     return msgs
   end
+end
+
+local function to_col_renderer_arg(node, index, focus, total, is_selected, ui, tree)
+  node.index = index
+  node.relative_index = math.abs(index - focus)
+  node.is_before_focus = index < focus
+  node.is_after_focus = index > focus
+  node.tree = tree
+  node.prefix = ui.prefix or ""
+  node.suffix = ui.suffix or ""
+  node.is_selected = is_selected
+  node.is_focused = index == focus
+  node.total = total
+  node.style = ui.style
+  node.meta = {}
+  return node
+end
+
+local function render_inactive_pane(ctx)
+  if state.inactive == nil then
+    return { CustomParagraph = "" }
+  end
+  local nodes = (state.inactive.directory_buffer or {}).nodes or {}
+  local total = #nodes
+  local focus = (state.inactive.directory_buffer or {}).focus or 0
+
+  local col_widths = xplr.config.general.table.col_widths
+  local cols = xplr.config.general.table.row.cols
+
+  local builtin = "builtin."
+  local custom = "custom."
+
+  local body = { {} }
+
+  for hi, header in ipairs(xplr.config.general.table.header.cols) do
+    body[1][hi] = header.format or ""
+  end
+
+  local is_selected = {}
+  for _, v in ipairs(ctx.app.selection) do
+    is_selected[v.absolute_path] = true
+  end
+
+  local visible = offset(nodes, ctx.layout_size.height, focus)
+  for ni, node in ipairs(visible) do
+    index = ni - 1
+    ni = ni + 1
+    local tree = xplr.config.general.table.tree
+    local t = tree[2].format or "├"
+    if index == 0 then
+      t = tree[1].format or "╭"
+    elseif index == total - 1 then
+      t = tree[3].format or "╰"
+    end
+
+    local ui = xplr.config.general.default_ui
+    if node.is_focused and node.is_selected then
+      ui = xplr.config.general.focus_selection_ui
+    elseif node.is_focused then
+      ui = xplr.config.general.focus_ui
+    elseif node.is_selected then
+      ui = xplr.config.general.selection_ui
+    end
+
+    local arg = to_col_renderer_arg(
+      node,
+      index,
+      focus,
+      total,
+      is_selected[node.absolute_path] or false,
+      ui,
+      t
+    )
+    body[ni] = {}
+    for ci, col in ipairs(cols) do
+      local render_name = col.format or ""
+      if string.sub(render_name, 1, #builtin) == builtin then
+        render_name = string.sub(render_name, #builtin + 1)
+        body[ni][ci] = xplr.fn.builtin[render_name](arg)
+      elseif string.sub(render_name, 1, #custom) == custom then
+        render_name = string.sub(render_name, #custom + 1)
+        body[ni][ci] = xplr.fn.custom[render_name](arg)
+      else
+        body[ni][ci] = "invalid renderer: " .. render_name
+      end
+    end
+  end
+
+  local dim = { add_modifiers = { "Dim" } }
+
+  local title = " "
+      .. xplr.util.shorten(state.inactive.pwd, { without_suffix_dots = true })
+      .. " ("
+      .. total
+      .. ")"
+
+  return {
+    CustomTable = {
+      ui = { title = { format = title, style = dim } },
+      widths = col_widths,
+      body = body,
+    },
+  }
 end
 
 local function setup(args)
@@ -172,9 +240,7 @@ local function setup(args)
 
   xplr.config.modes.builtin.default.key_bindings.on_key.q = on_key.q
 
-  local default_layout = xplr.config.layouts.builtin.default
   xplr.config.layouts.custom.left_pane_active = split_pane(
-    default_layout,
     args.active_pane_width,
     args.inactive_pane_width,
     "Table",
@@ -182,7 +248,6 @@ local function setup(args)
   )
 
   xplr.config.layouts.custom.right_pane_active = split_pane(
-    default_layout,
     args.inactive_pane_width,
     args.active_pane_width,
     inactive_pane_layout,
@@ -190,41 +255,7 @@ local function setup(args)
   )
 
   xplr.fn.custom.dual_pane = {}
-  xplr.fn.custom.dual_pane.render_inactive_pane = function(ctx)
-    local tree = xplr.config.general.table.tree
-    if dual_pane == nil or dual_pane.inactive.directory_buffer == nil then
-      return {}
-    else
-      local buf = dual_pane.inactive.directory_buffer
-      local res = {
-        buf.parent .. " (" .. buf.total .. ")",
-      }
-
-      local h = ctx.layout_size.height - 3
-      local start = (buf.focus - (buf.focus % h))
-      for i = start + 1, start + h, 1 do
-        if i > buf.total then
-          break
-        end
-        local node = dual_pane.inactive.directory_buffer.nodes[i]
-        local path = node.relative_path
-        if i == buf.total then
-          path = (tree[3].format or "") .. " " .. path
-        elseif i == 1 then
-          path = (tree[1].format or "") .. " " .. path
-        else
-          path = (tree[2].format or "") .. " " .. path
-        end
-
-        if node.is_dir then
-          path = path .. "/"
-        end
-
-        table.insert(res, path)
-      end
-      return res
-    end
-  end
+  xplr.fn.custom.dual_pane.render_inactive_pane = render_inactive_pane
 
   xplr.fn.custom.dual_pane.activate_left_pane = function(ctx)
     return activate_pane(pane.LEFT, ctx)
@@ -235,7 +266,7 @@ local function setup(args)
   end
 
   xplr.fn.custom.dual_pane.toggle_pane = function(ctx)
-    if dual_pane and dual_pane.active == pane.RIGHT then
+    if state.active == pane.RIGHT then
       return xplr.fn.custom.dual_pane.activate_left_pane(ctx)
     else
       return xplr.fn.custom.dual_pane.activate_right_pane(ctx)
@@ -243,10 +274,10 @@ local function setup(args)
   end
 
   xplr.fn.custom.dual_pane.quit_active_pane = function(ctx)
-    if dual_pane then
+    if state.active then
       local msgs = xplr.fn.custom.dual_pane.toggle_pane(ctx)
       table.insert(msgs, { SwitchLayoutBuiltin = "default" })
-      dual_pane = nil
+      state.active = nil
       return msgs
     else
       return {
@@ -256,4 +287,6 @@ local function setup(args)
   end
 end
 
-return { setup = setup }
+-- return { setup = setup }
+version = "0.21.1"
+setup()
